37f46688885eb773c8494788a52ed8d4d663a774
hanee9492 <hanee9492@gmail.com>
2022-12-08 22:32:00 +0900

    3.13. Implementing Order Service domain logic using DDD - Order Aggregate Root

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/order/system/order/service/domain/valueobject/OrderItemId.java
@@ -0,0 +1,11 @@
+package com.food.order.system.order.service.domain.valueobject;
+
+import com.food.order.system.valueobject.BaseId;
+
+public class OrderItemId extends BaseId<Long> {
+
+
+  protected OrderItemId(Long value) {
+    super(value);
+  }
+}
